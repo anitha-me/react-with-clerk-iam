97cde8a14c0f60d9241355b8ac46ac75ba15ec12
Anithabtc <anitha.me@bigthinkcode.com>
2025-02-18 08:56:37 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,48 @@
+import { useTranslation } from 'react-i18next';
 import reactLogo from './assets/react.svg';
+import { i18n } from './lib/i18n/i18n';
 import viteLogo from '/vite.svg';
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from '@clerk/clerk-react';
 
-function App() {
+export default function App() {
+  const changeLanguage = (lang: string) => {
+    i18n.changeLanguage(lang);
+  };
+
+  const { t } = useTranslation();
   return (
-    <div className='h-screen w-screen flex justify-center items-center text-[5vw] md:text-[3vw] lg:text-[2.5vw] text-center gap-4 text-while'>
-      <div>
-        <img src={viteLogo} className='logo' alt='Vite logo' />
-      </div>
-      <div>
-        <img src={reactLogo} className='logo react' alt='React logo' />
-      </div>
-      <div>React + Vite js + Tailwind CSS</div>
-    </div>
+    <>
+      <main className='h-screen w-screen flex flex-col justify-center items-center gap-10'>
+        <div className='flex gap-4 flex-wrap'>
+          <button onClick={() => changeLanguage('en')}>English</button>
+          <button onClick={() => changeLanguage('ja')}>Japanese</button>
+          <button onClick={() => changeLanguage('ko')}>Korean</button>
+          <button onClick={() => changeLanguage('ta')}>Tamil</button>
+        </div>
+        {t('text')}
+        <div>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+        <div className='flex gap-4 items-center text-center text-[5vw] md:text-[3vw] lg:text-[2.5vw]'>
+          <div>
+            <img src={viteLogo} className='logo' alt='Vite logo' />
+          </div>
+          <div>
+            <img src={reactLogo} className='logo react' alt='React logo' />
+          </div>
+          <div>React + Vite js + Tailwind CSS</div>
+        </div>
+      </main>
+    </>
   );
 }
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import { AppProvider } from './providers/index.tsx';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AppProvider>
   </StrictMode>
 );
